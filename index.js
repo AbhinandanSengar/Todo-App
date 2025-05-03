c11e1d52f304f8ce329c602cb25c340320e7b6e7
AbhinandanSengar <abhinandansengar@gmail.com>
2025-05-04 01:27:59 +0530

    Updated index.js

--- index.js
+++ index.js
@@ -34,7 +34,8 @@ app.post('/signup', function(req, res) {
         name: name,
         username: username,
         email: email,
-        password: password
+        password: password,
+        todos: []
     })
 
     res.send({
@@ -114,18 +115,75 @@ app.get('/profile', auth, function(req, res) {
 })
 
 //Todo Route
+//Adding a Todo
+app.post('/todos', auth, function(req, res) {
+    const userDetails = users.find(user => user.username === req.username);
+    const todoTitle = req.body.todoTitle;
+
+    if(!todoTitle) {
+        res.status(400).send({
+            message: "Please enter a new todo"
+        });
+        return;
+    }
+
+    const todo = {
+        id: Date.now(),
+        todoTitle,
+        completed: false
+    }
+
+    userDetails.todos.push(todo);
+
+    res.send({
+        message: "Todo added!", todo
+    })
+})
+
+//Getting a Todo
 app.get('/todos', auth, function(req, res) {
     const userDetails = users.find(user => user.username === req.username);
 
-    if(userDetails) {
-        res.send({
-            message: "Hi" + userDetails.username
-        })
-    } else {
-        res.status(404).send({
-            message: "user not found!"
-        })
-    }
+    res.send({
+        todos: userDetails.todos
+    })
 })
 
-app.listen(PORT);
+//marking a Todo as DONE
+app.put('/todos/:id/done', auth, function(req, res) {
+    const userDetails = users.find(user => user.username === req.username);
+    const todo = userDetails.todos.find(td => td.id === Number(req.params.id));
+
+    if(!todo) {
+        res.status(404).send({
+            message: "Todo not found"
+        });
+        return;
+    }
+
+    todo.completed = true;
+
+    res.send({
+        message: "Marked as DONE!", todo
+    })
+})
+
+app.delete('/todos/:id', auth, function(req, res) {
+    const userDetails = users.find(user => user.username === req.username);
+    const todoId = userDetails.todos.findIndex(todo => todo.id === Number(req.params.id));
+
+    if(todoId === -1) {
+        res.status(404).send({
+            message: "Todo not found"
+        })
+        return;
+    }
+
+    userDetails.todos.splice(todoId, 1);
+
+    res.send({
+        message: "Todo deleted!"
+    })
+})
+
+app.listen(3000);
